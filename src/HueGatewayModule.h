1de7add6400f0c6e496595a9cd2b111d3f271966
estebanri87 <42171761+estebanri87@users.noreply.github.com>
2026-03-04 11:39:11 +0100

    chore(release): bump module version to 0.3.2

--- src/HueGatewayModule.h
+++ src/HueGatewayModule.h
@@ -23,8 +23,8 @@ struct HueGatewayEventLightUpdate;
  * It communicates with the Hue Bridge via Hue API v2 and exposes device
  * functions through KNX communication objects.
  * 
- * @version 0.3.1
- * @date 2026-02-28
+ * @version 0.3.2
+ * @date 2026-04-04
  */
 
 class HueGatewayModule : public OpenKNX::Module
